a7a8aa9db968b1c630cbc152cd4f95c07426a2de
franciscobaez <jessiebaez@outlook.com>
2024-09-19 13:28:19 -0400

    progress

---
+++ pokemon_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""comment"""
+
+
+class Pokemon:
+    """Pokemon class"""
+    def __init__(self, name, owner, type):
+        """comment"""
+        self.name = name
+        self.owner = owner
+        self.type = type
+
+    
