a45eef1867a7a8113bf7591d2f8beaa54087cea0
Francisco Antonio Baez Lopez <jessiebaez316@outlook.com>
2025-01-03 16:50:36 -0400

    image reading testing files

--- salsa project testing/pylance.py
+++ salsa project testing/pylance.py
@@ -1,3 +1,9 @@
-import pylance
+from PIL import Image
+import pytesseract
 
+# Open an image file
+image = Image.open('4.jpeg')
 
+# Extract text
+text = pytesseract.image_to_string(image)
+print(text)
